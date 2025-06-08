114c44e7d23f06cdbcb1367130b7300d918d824b
Ahmed101Noor <78204155+Ahmed101Noor@users.noreply.github.com>
2025-06-08 22:21:38 +0300

    Updated colors of category design

--- Stationery_Store/Forms/CategoryForm.Designer.cs
+++ Stationery_Store/Forms/CategoryForm.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
-            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
             label1 = new Label();
             textBox1 = new TextBox();
             label2 = new Label();
@@ -48,79 +48,82 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Arial Narrow", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(256, 24);
+            label1.Font = new Font("Microsoft Sans Serif", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(224, 18);
             label1.Name = "label1";
-            label1.Size = new Size(181, 43);
+            label1.Size = new Size(145, 33);
             label1.TabIndex = 0;
             label1.Text = "اضافة صنف";
             // 
             // textBox1
             // 
+            textBox1.BorderStyle = BorderStyle.FixedSingle;
             textBox1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
             textBox1.ForeColor = Color.Black;
-            textBox1.Location = new Point(643, 136);
-            textBox1.Margin = new Padding(3, 4, 3, 4);
+            textBox1.Location = new Point(591, 102);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(257, 47);
+            textBox1.PlaceholderText = "اكتب اسم الصنف هنا";
+            textBox1.Size = new Size(225, 39);
             textBox1.TabIndex = 2;
+            textBox1.TextAlign = HorizontalAlignment.Center;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Font = new Font("Arial Narrow", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label2.Location = new Point(746, 88);
+            label2.Font = new Font("Microsoft Sans Serif", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(633, 66);
             label2.Name = "label2";
-            label2.Size = new Size(168, 43);
+            label2.Size = new Size(135, 33);
             label2.TabIndex = 0;
             label2.Text = "اسم الصنف";
             // 
             // UpdateCategoryBtn
             // 
-            UpdateCategoryBtn.Font = new Font("Arial Rounded MT Bold", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            UpdateCategoryBtn.Location = new Point(31, 528);
-            UpdateCategoryBtn.Margin = new Padding(3, 4, 3, 4);
+            UpdateCategoryBtn.BackColor = Color.DodgerBlue;
+            UpdateCategoryBtn.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            UpdateCategoryBtn.Location = new Point(27, 396);
             UpdateCategoryBtn.Name = "UpdateCategoryBtn";
-            UpdateCategoryBtn.Padding = new Padding(2, 3, 2, 3);
-            UpdateCategoryBtn.Size = new Size(272, 56);
+            UpdateCategoryBtn.Padding = new Padding(2);
+            UpdateCategoryBtn.Size = new Size(238, 42);
             UpdateCategoryBtn.TabIndex = 1;
             UpdateCategoryBtn.Text = "تحديث صنف";
-            UpdateCategoryBtn.UseVisualStyleBackColor = true;
+            UpdateCategoryBtn.UseVisualStyleBackColor = false;
             UpdateCategoryBtn.Click += UpdateCategoryBtn_Click;
             // 
             // RemoveCategoryBtn
             // 
-            RemoveCategoryBtn.Font = new Font("Arial Rounded MT Bold", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            RemoveCategoryBtn.Location = new Point(310, 528);
-            RemoveCategoryBtn.Margin = new Padding(3, 4, 3, 4);
+            RemoveCategoryBtn.BackColor = Color.Red;
+            RemoveCategoryBtn.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            RemoveCategoryBtn.Location = new Point(271, 396);
             RemoveCategoryBtn.Name = "RemoveCategoryBtn";
-            RemoveCategoryBtn.Padding = new Padding(2, 3, 2, 3);
-            RemoveCategoryBtn.Size = new Size(272, 56);
+            RemoveCategoryBtn.Padding = new Padding(2);
+            RemoveCategoryBtn.Size = new Size(238, 42);
             RemoveCategoryBtn.TabIndex = 1;
             RemoveCategoryBtn.Text = "ازالة صنف";
-            RemoveCategoryBtn.UseVisualStyleBackColor = true;
+            RemoveCategoryBtn.UseVisualStyleBackColor = false;
             RemoveCategoryBtn.Click += RemoveCategoryBtn_Click;
             // 
             // AddCategoryBtn
             // 
-            AddCategoryBtn.Font = new Font("Arial Rounded MT Bold", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            AddCategoryBtn.Location = new Point(592, 528);
-            AddCategoryBtn.Margin = new Padding(3, 4, 3, 4);
+            AddCategoryBtn.BackColor = Color.Green;
+            AddCategoryBtn.FlatAppearance.BorderSize = 0;
+            AddCategoryBtn.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            AddCategoryBtn.Location = new Point(518, 396);
             AddCategoryBtn.Name = "AddCategoryBtn";
-            AddCategoryBtn.Padding = new Padding(2, 3, 2, 3);
-            AddCategoryBtn.Size = new Size(272, 56);
+            AddCategoryBtn.Padding = new Padding(2);
+            AddCategoryBtn.Size = new Size(238, 42);
             AddCategoryBtn.TabIndex = 1;
             AddCategoryBtn.Text = "اضافة صنف";
-            AddCategoryBtn.UseVisualStyleBackColor = true;
+            AddCategoryBtn.UseVisualStyleBackColor = false;
             AddCategoryBtn.Click += AddCategoryBtn_Click;
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Font = new Font("Arial Narrow", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label3.Location = new Point(643, 215);
+            label3.Font = new Font("Microsoft Sans Serif", 21.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(591, 161);
             label3.Name = "label3";
-            label3.Size = new Size(285, 43);
+            label3.Size = new Size(225, 33);
             label3.TabIndex = 0;
             label3.Text = "معلومات عن الصنف";
             // 
@@ -128,51 +131,52 @@
             // 
             CategoryStatuslbl.AutoSize = true;
             CategoryStatuslbl.ForeColor = Color.Green;
-            CategoryStatuslbl.Location = new Point(561, 44);
+            CategoryStatuslbl.Location = new Point(491, 33);
             CategoryStatuslbl.Name = "CategoryStatuslbl";
-            CategoryStatuslbl.Size = new Size(0, 20);
+            CategoryStatuslbl.Size = new Size(0, 15);
             CategoryStatuslbl.TabIndex = 4;
             // 
             // textBox2
             // 
+            textBox2.BackColor = SystemColors.ButtonHighlight;
+            textBox2.BorderStyle = BorderStyle.FixedSingle;
             textBox2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox2.Location = new Point(643, 263);
-            textBox2.Margin = new Padding(3, 4, 3, 4);
+            textBox2.Location = new Point(591, 197);
             textBox2.Multiline = true;
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(257, 256);
+            textBox2.PlaceholderText = "اكتب معلومات عن الصنف هنا";
+            textBox2.Size = new Size(225, 193);
             textBox2.TabIndex = 5;
+            textBox2.TextAlign = HorizontalAlignment.Right;
             // 
             // dataGridView1
             // 
-            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleRight;
-            dataGridViewCellStyle1.BackColor = SystemColors.Control;
-            dataGridViewCellStyle1.Font = new Font("Segoe UI", 9F);
-            dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
-            dataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
-            dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridView1.BackgroundColor = SystemColors.GradientActiveCaption;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleRight;
+            dataGridViewCellStyle2.BackColor = SystemColors.Control;
+            dataGridViewCellStyle2.Font = new Font("Segoe UI", 9F);
+            dataGridViewCellStyle2.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
+            dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(14, 88);
-            dataGridView1.Margin = new Padding(3, 4, 3, 4);
+            dataGridView1.Location = new Point(12, 66);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RightToLeft = RightToLeft.Yes;
             dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(623, 432);
+            dataGridView1.Size = new Size(545, 324);
             dataGridView1.TabIndex = 6;
             dataGridView1.CellContentClick += dataGridView1_CellContentClick;
             dataGridView1.Click += dataGridView1_SelectionChanged;
             // 
-            // categoryBindingSource
-            // 
-            categoryBindingSource.DataSource = typeof(Entities.Category);
-            // 
             // CategoryForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1010, 608);
+            AutoValidate = AutoValidate.EnableAllowFocusChange;
+            BackColor = SystemColors.GradientInactiveCaption;
+            ClientSize = new Size(884, 456);
             Controls.Add(dataGridView1);
             Controls.Add(textBox2);
             Controls.Add(CategoryStatuslbl);
@@ -183,7 +187,6 @@
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
-            Margin = new Padding(3, 4, 3, 4);
             Name = "CategoryForm";
             Text = "الأصناف";
             Load += CategoryForm_Load;
